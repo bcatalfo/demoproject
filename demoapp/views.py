7c05f0f2550db3cc683dc2f00c934a80a111a3d8
Benjamin Catalfo <bcatalfo@gmail.com>
2023-03-11 11:40:52 -0500

    Add an http response.
    Uses django's HttpResponse to send a Hello, World message.

--- demoapp/views.py
+++ demoapp/views.py
@@ -1,3 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django import HttpResponse
+def index(request):
+    return HttpResponse("Hello, world. This is the index view of Demoapp")
