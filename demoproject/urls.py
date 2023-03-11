2a5712fd91cbc74476bf737999043dec1cd6c6fb
Benjamin Catalfo <bcatalfo@gmail.com>
2023-03-11 15:24:42 -0500

    added home page

--- demoproject/urls.py
+++ demoproject/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from demoapp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('demo/', include('demoapp.urls')),
+    path("admin/", admin.site.urls),
+    path("demo/", include("demoapp.urls")),
+    path("", views.home, name="home"),
 ]
