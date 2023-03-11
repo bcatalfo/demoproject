0944e979603bd0414b69bddfa51f2a2294977c8f
Benjamin Catalfo <bcatalfo@gmail.com>
2023-03-11 15:24:42 -0500

    Add home page.
    The home function in demoapp returns an HttpResponse that welcomes the
    user. Demoproject imports views from demoapp and then links the home
    page to the home function.

--- demoapp/views.py
+++ demoapp/views.py
@@ -1,3 +1,9 @@
 from django.http import HttpResponse
+
+
 def index(request):
     return HttpResponse("Hello, world. This is the index view of Demoapp")
+
+
+def home(request):
+    return HttpResponse("Hello- this is the home page!")
