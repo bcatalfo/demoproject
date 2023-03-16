3c6fbee1db842cced8a36e7b056b80d297191ff2
Benjamin Catalfo <bcatalfo@gmail.com>
2023-03-16 15:46:00 -0400

    Add customer and vehicle models.

--- demoapp/models.py
+++ demoapp/models.py
@@ -28,3 +28,14 @@ class Person(models.Model):
     email = models.EmailField()
     phone = models.CharField(max_length=20)
     age = models.IntegerField(null=True)
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Vehicle(models.Model):
+    name = models.CharField(max_length=255)
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE, related_name="Vehicle"
+    )
