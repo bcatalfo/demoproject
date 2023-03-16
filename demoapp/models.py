b064752b8201099221346969b2cbfd9976596ad9
Benjamin Catalfo <bcatalfo@gmail.com>
2023-03-16 14:43:12 -0400

    Add menu category.

--- demoapp/models.py
+++ demoapp/models.py
@@ -4,10 +4,17 @@ from django.db import models
 # Create your models here.
 
 
+class MenuCategory(models.Model):
+    menu_category_name = models.CharField(max_length=200)
+
+
 class Menu(models.Model):
     name = models.CharField(max_length=100)
     cuisine = models.CharField(max_length=100)
-    price = models.IntegerField()
+    price = models.IntegerField(null=False)
+    category_id = models.ForeignKey(
+        MenuCategory, on_delete=models.PROTECT, default=None
+    )
 
     def __str__(self):
         return self.name + " : " + self.cuisine
