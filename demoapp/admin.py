72c1397baec43e7013e436e3d8ef3042bb5cb1cf
Benjamin Catalfo <bcatalfo@gmail.com>
2023-03-17 15:08:02 -0400

    Disable editing date joined from the admin panel.

--- demoapp/admin.py
+++ demoapp/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 from .models import Menu, MenuCategory
 
 # Register your models here.
 admin.site.register(Menu)
 admin.site.register(MenuCategory)
+
+# Unregister the provided model admin
+admin.site.unregister(User)
+
+
+@admin.register(User)
+class NewAdmin(UserAdmin):
+    readonly_fields = [
+        "date_joined",
+    ]
