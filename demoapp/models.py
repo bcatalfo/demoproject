232fbe0e219fe8c45ed057bb4653fb59729ce7fa
Benjamin Catalfo <bcatalfo@gmail.com>
2023-03-17 15:44:23 -0400

    Make Persons display as their name.
    By default you just see the object and an id.

--- demoapp/models.py
+++ demoapp/models.py
@@ -29,6 +29,9 @@ class Person(models.Model):
     phone = models.CharField(max_length=20)
     age = models.IntegerField(null=True)
 
+    def __str__(self):
+        return self.Person_name
+
 
 class Customer(models.Model):
     name = models.CharField(max_length=255)
