a40933ae218991ce31ef37dd360bf4ed23dd1ba9
Benjamin Catalfo <bcatalfo@gmail.com>
2023-03-28 17:37:53 -0400

    Move templates folder to parent directory.
    The templates folder was (mistakenly) originally placed at
    demoproject/templates. The parent folder for all of this on my machine
    which you can't see if also called demoproject, leading to the confusion.

--- demoproject/settings.py
+++ demoproject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "demoproject.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "demoproject/templates"],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
